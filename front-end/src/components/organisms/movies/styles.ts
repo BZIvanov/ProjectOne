70c0c75e0c778e61002ad76035eedca9eed9fe53
BZIvanov <ivanov.biser@gmx.com>
2020-05-30 19:42:08 +0300

    added forms and router package

--- front-end/src/components/organisms/movies/styles.ts
+++
@@ -1,8 +0,0 @@
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-
-export const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-    backgroundColor: theme.palette.primary.dark,
-    border: '5px solid black'
-  },
-}));
