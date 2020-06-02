953ad1fb204940c2045656f0f2720168048c72bf
BZIvanov <ivanov.biser@gmx.com>
2020-06-02 22:04:03 +0300

    added active link styles

--- client/src/components/atoms/heading/index.jsx
+++ client/src/components/atoms/heading/index.jsx
@@ -4,26 +4,32 @@ import styled from 'styled-components';
 const HeadingOne = styled.h1`
   font-size: 2.5rem;
   line-height: 1.5;
+  color: ${({ color, theme }) => color || theme.palette.a};
 `;
-const HeadingTwo = styled.h1`
+const HeadingTwo = styled.h2`
   font-size: 2.2rem;
   line-height: 1.5;
+  color: ${({ color, theme }) => color || theme.palette.a};
 `;
-const HeadingThree = styled.h1`
+const HeadingThree = styled.h3`
   font-size: 1.8rem;
   line-height: 1.5;
+  color: ${({ color, theme }) => color || theme.palette.a};
 `;
-const HeadingFour = styled.h1`
+const HeadingFour = styled.h4`
   font-size: 1.6rem;
   line-height: 1.5;
+  color: ${({ color, theme }) => color || theme.palette.a};
 `;
-const HeadingFive = styled.h1`
+const HeadingFive = styled.h5`
   font-size: 1.4rem;
   line-height: 1.5;
+  color: ${({ color, theme }) => color || theme.palette.a};
 `;
-const HeadingSix = styled.h1`
+const HeadingSix = styled.h6`
   font-size: 1.2rem;
   line-height: 1.5;
+  color: ${({ color, theme }) => color || theme.palette.a};
 `;
 
 const Heading = ({ variant, children }) => {
