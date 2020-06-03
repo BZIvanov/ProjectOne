eef0353ec52529f19c336e801106c3044c37cc73
BZIvanov <ivanov.biser@gmx.com>
2020-06-03 22:49:18 +0300

    authenticated links

--- client/src/components/molecules/links/index.jsx
+++ client/src/components/molecules/links/index.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavItems, NavItem, NavLink } from '../../atoms';
 
 const Links = () => {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setAuth(true);
+    }
+  }, []);
+
   return (
     <NavItems>
-      <NavItem>
-        <NavLink to="/movie/add">Add Movie</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink to="/signup">Signup</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink to="/signin">Signin</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink to="/logout">Logout</NavLink>
-      </NavItem>
+      {auth ? (
+        <>
+          <NavItem>
+            <NavLink to="/movie/add">Add Movie</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/logout">Logout</NavLink>
+          </NavItem>
+        </>
+      ) : (
+        <>
+          <NavItem>
+            <NavLink to="/signup">Signup</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/signin">Signin</NavLink>
+          </NavItem>
+        </>
+      )}
     </NavItems>
   );
 };
